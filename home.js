00b8f296153e6aa0042393545d14b1a1758eed11
Irulian <79067844+Irulian@users.noreply.github.com>
2022-03-18 10:44:07 -0600

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
 window.onbeforeunload = function () {
-  return true;
-}
+  return true
+};
+function openBib() {
+  let bib = document.getElementById('bib-div')
+  bib.style.visibility = 'visible'
+};
+function closeBib() {
+  let bib = document.getElementById('bib-div')
+  bib.style.visibility = 'hidden'
+};
